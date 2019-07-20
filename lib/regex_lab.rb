1743f71c2ca52e2330639b5ec53bc2d99d026c31
Emily Lillie <emilylillie3@gmail.com>
2019-07-20 20:14:36 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,9 @@
 def starts_with_a_vowel?(word)
-  word.match (/^\[aeiou]/)
+  if word.match(/^[aeiouAEIOU]\w+/) == nil
+    return false
+  else
+    return true 
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
